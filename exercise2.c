713fed21f6a823979aaf05fbdc2915c655e3e379
visualne <none@MacBook-Air.local>
2022-09-17 23:05:47 -0400

    simple exercises

---
+++ exercise2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char ch;
+    
+    // Reading input.
+    scanf("%c", &ch);
+    printf("%c", ch);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
